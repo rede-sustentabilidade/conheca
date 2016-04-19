f417a6904d54edae462e7131c9bdd940b11f9a54
Lucas Pirola <lucaspirola@gmail.com>
2016-04-19 12:27:34 -0300

    Melhorias no mobile

--- src/components/YoutubePlayer.js
+++ src/components/YoutubePlayer.js
@@ -1,20 +1,26 @@
 import React, { PropTypes } from 'react';
+import MediaQuery from 'react-responsive';
 
 // Since this component is simple and static, there's no parent container for it.
 const YoutubePlayer = ({id, playVideo, appState}) => {
 
     const thumbnail = 'url(//i.ytimg.com/vi/' + id + '/maxresdefault.jpg)';
+    const embed = "//www.youtube.com/embed/" + id + "?autoplay=1&autohide=1&modestbranding=1&showinfo=0&controls=0&vq=hd720";
+    let size = { width: '100%', height: '555px'};
 
-    const size = {
-      width: '100%',
-      height: '555px'
-    };
+    if (window.matchMedia("(max-width: 768px)").matches) {
+      size = { width: '100%', height: '205px'};
+    }
 
     return (<div className="video-featured">
       {
         !appState.isPlaying
-        ? <div className="youtube" style={{...size, backgroundImage: thumbnail}}><div className="play" onClick={playVideo}></div></div>
-      : <iframe frameBorder="0" src={"//www.youtube.com/embed/" + id + "?autoplay=1&autohide=1&modestbranding=1&showinfo=0&controls=0&vq=hd720"} style={size} />
+        ? <div className="youtube" style={{...size, backgroundImage: thumbnail}}>
+            <div className="play" onClick={playVideo}>
+              <i className="fa fa-play-circle"></i>
+            </div>
+          </div>
+        : <iframe frameBorder="0" src={embed} style={size} />
       }
     </div>);
 };
@@ -22,7 +28,8 @@ const YoutubePlayer = ({id, playVideo, appState}) => {
 YoutubePlayer.propTypes = {
   id: PropTypes.string.isRequired,
   playVideo: PropTypes.func.isRequired,
-  appState: PropTypes.object.isRequired
+  appState: PropTypes.object.isRequired,
+  isMobile: PropTypes.bool
 };
 
 export default YoutubePlayer;
